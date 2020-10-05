b206a88fc748d6f94110efac1a3655de1e797876
Manoel Carvalho <manoelmecat@gmail.com>
2020-10-05 20:01:15 -0300

    first commit

---
+++ data/siteConfig.js
@@ -0,0 +1,139 @@
+module.exports = {
+  siteTitle: 'OI! Eu sou o Manolo!',
+  siteDescription: `Meu site portifólio`,
+  keyWords: ['gatsbyjs', 'react', 'react native', 'nodejs'],
+  authorName: 'Manoel Carvalho',
+  twitterUsername: 'manolocarvaljo',
+  githubUsername: 'TheNeoCarvalho',
+  authorAvatar: 'https://media-exp1.licdn.com/dms/image/C5603AQFeV_afcKuwbQ/profile-displayphoto-shrink_200_200/0?e=1607558400&v=beta&t=Sf8_g0n2-3p7Y6iuC2UKDC879eNxbXMHXwM4MFfgijs',
+  authorDescription: `Estudante e amante de novas tecnologias.
+                      <p>Professor/Coordenador na EEEP Profª Maria Célia Pinheiro Falção.</p>
+                      <p>FullStack Developer JS</p>
+                      <ul>
+                      <li><span>NodeJS</span></li>
+                      <li><span>ReactJS</span></li>
+                      <li><span>React Native</span></li>
+                      </ul>
+                     `,
+  skills: [
+    {
+      name: 'HTML5',
+      level: 70
+    },
+    {
+      name: 'CSS3',
+      level: 60
+    },
+    {
+      name: 'Javascript',
+      level: 80
+    },
+    {
+      name: 'NodeJs',
+      level: 70
+    },
+    {
+      name: 'React',
+      level: 70
+    },
+    {
+      name: 'React Native',
+      level: 70
+    },
+    {
+      name: 'Git',
+      level: 70
+    },
+    {
+      name: 'PHP',
+      level: 80
+    },
+    {
+      name: 'Laravel',
+      level: 70
+    },
+    /* more skills here */
+  ],
+  jobs: [
+    /* more jobs here */
+    {
+      company: "EEEP Profª Maria Célia Pinheiro Falcão",
+      begin: {
+        month: 'set',
+        year: '2012'
+      },
+      duration: null,
+      occupation: "Professor EMI",
+      description: "Atualmente ministrando as disciplinas: Lógica de Programação, Programação Web I e II, Banco de Dados, PHP e MySQL."
+
+    }, {
+      company: "FVS - Faculdade Vale do Salgado",
+      begin: {
+        month: 'mar',
+        year: '2012'
+      },
+      duration: '1 ano e 3 meses',
+      occupation: "Professor Universitário",
+      description: "Ministrava as disciplinas: Programação para Internet, Redes de Computadores, Organização e Arquitetura de Computadores."
+
+    }, {
+      company: "Start Informática",
+      begin: {
+        month: 'jan',
+        year: '2012'
+      },
+      duration: '2 anos',
+      occupation: "Desenvolvedor Fullstack",
+      description: "Trabalhava com as tecnologias Web, HTML/CSS, PHP e MySQL, Joomla, Wordpress."
+    }, {
+      company: "COPY VIP",
+      begin: {
+        month: 'set',
+        year: '2010'
+      },
+      duration: '1 anos e 6 meses',
+      occupation: "Suporte Técnico",
+      description: "Instalação e manutenção preventiva e corretiva de impressoras."
+
+    },
+  ],
+  portifolio: [
+    {
+      image: "https://balloondesignbrasil.com.br/wp-content/uploads/2016/08/aguarde-white.png",
+      description: "Em breve",
+      url: "#"
+    },
+    {
+      image: "https://balloondesignbrasil.com.br/wp-content/uploads/2016/08/aguarde-white.png",
+      description: "Em breve",
+      url: "#"
+    },
+    /* more portifolio items here */
+  ],
+  social: {
+    twitter: "https://twitter.com/neo_carvalho",
+    linkedin: "https://www.linkedin.com/in/franciscomanoelcarvalho",
+    github: "https://github.com/TheNeoCarvalho",
+    email: "manoelmecat@gmail.com"
+  },
+  siteUrl: 'https://TheNeoCarvalho.github.io/manoloblog',
+  pathPrefix: '/manoloblog', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpg',
+  googleAnalyticsId: 'UA-000000000-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  fontColor: "#000000cc",
+  enableDarkmode: true, // If true, enables dark mode switch
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Home',
+      url: '/',
+    },
+    {
+      label: 'Portifolio',
+      url: '/portifolio',
+    }
+  ]
+}
